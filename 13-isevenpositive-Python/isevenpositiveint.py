be2175338e2fac9dc4204784474a0cef8e17d5f0
kiranreddydugga <72123425+kiranreddydugga@users.noreply.github.com>
2021-08-05 10:25:12 +0530

    kiranmayi

--- 13-isevenpositive-Python/isevenpositiveint.py
+++ 13-isevenpositive-Python/isevenpositiveint.py
@@ -4,10 +4,24 @@
 # crashing if the value is not an integer. So, isevenpositiveint("yikes!") returns False (rather
 # than crashing), and isevenpositiveint(123456) returns True.
 
-def isevenpositiveint(x):
-	# your code goes here
-     if type(x) == int and x>0 and x%2 ==0:
-        return True
-     else:
-        return False
-	
+def getEvenDigits(n,i=0,x=0):
+    if(n == 0):
+        return x
+    else:
+        rem = n%10
+        if(rem % 2 == 0):
+            x += rem*(10**(i))
+            i+=1 
+        return getEvenDigits(n//10,i,x)
+        
+def onlyEvenDigitsHelper(l,res=[]):
+    if(l == []):
+        return res
+    else:
+        res.append(getEvenDigits(l[0]))
+        return onlyEvenDigitsHelper(l[1:],res)
+        
+def fun_recursion_onlyevendigits(l): 
+    if(l == []):
+        return []
+    return onlyEvenDigitsHelper(l,[])
